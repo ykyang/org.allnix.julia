74b4ac1ab043a87625eb684015912359a0c938c7
Yi-Kun Yang <ykyang@gmail.com>
2017-05-22 21:30:14 -0500

    Add chapter 3 study

---
+++ ch3.jl
@@ -0,0 +1,21 @@
+function insert_element(values)
+  push!(values, -10)
+end
+
+list = [1,2,3]
+insert_element(list)
+println(list)
+
+f(x,y) = x^3 + x*y - y
+g(a, b = 5) = a + b
+k(x; a1=1, a2=2) = x * (a1+a2)
+
+println(f(3,2))
+
+println("g(3) + f(1,2) = $( g(3) + f(1,2) )")
+println("k(2) = $(k(2))")
+
+cubes = map(x->x^3, [1,2,3,5])
+println("$cubes")
+
+println("$([1:3])")
