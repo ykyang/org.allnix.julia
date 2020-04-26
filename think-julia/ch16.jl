2f62830b8fdd5b9bf6c719f30a0f28029c3ee8b1
Yi-Kun Yang <ykyang@gmail.com>
2020-04-26 12:37:53 -0500

    Think Julia:Chapter 16. Structs and Functions

---
+++ think-julia/ch16.jl
@@ -0,0 +1,55 @@
+# Structs and Functions
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#chap16
+#include("ch15module.jl")
+include("ch16module.jl")
+import .AllnixThinkJulia
+ns = AllnixThinkJulia
+
+# functional programming style
+
+# Time
+time = ns.MyTime(11, 59, 30)
+# Exercise 16-1
+ns.printtime(time)
+# Exercise 16-2
+t1 = ns.MyTime(9, 30, 15)
+t2 = ns.MyTime(8, 45, 30)
+ns.isafter(t2, t1)
+
+# Pure Functions
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#_pure_functions
+
+# prototype and patch
+
+# A pure function
+start = ns.MyTime(9, 45, 0)
+duration = ns.MyTime(1, 35, 0)
+done = ns.addtime(start, duration)
+
+ns.printtime(done)
+
+# Modifiers
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#modifiers
+
+# Function that modifies its parameter
+
+# Exercise 16-3: See ch16module.jl
+time = ns.MMyTime(9, 45, 0)
+ns.increment!(time, 130)
+ns.printtime(time)
+
+# Exercise 16-4: See ch16module.jl
+time = ns.MyTime(9, 45, 0)
+time = ns.increment(time, 130)
+ns.printtime(time)
+
+# Prototyping Versus Planning
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#prototyping_versus_planning
+
+# prototype and patch: could be messy
+# designed development: more insight into the problem then do it
+time = ns.MyTime(9, 45, 0)
+totalsecond = ns.timetosecond(time)
+time = ns.secondtotime(totalsecond)
+
+# Exercise 16-5: See ch16module.jl
---
+++ think-julia/ch16module.jl
@@ -0,0 +1,142 @@
+# Structs and Functions
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#chap16
+#include("ch15module.jl")
+module AllnixThinkJulia
+
+using Printf
+
+# Time
+"""
+Represents the time of day.
+
+hour:
+minute:
+second:
+"""
+struct MyTime
+    hour
+    minute
+    second
+end
+
+# Exercise 16-1
+function printtime(mytime)
+    @printf("%02d:%02d:%02d\n", mytime.hour, mytime.minute, mytime.second)
+end
+
+# Exercise 16-2
+function isafter(t2, t1)
+    tosecond(t2) < tosecond(t1)
+end
+function tosecond(mytime)
+    mytime.hour*60*60 + mytime.minute*60 + mytime.second
+end
+
+# Pure Function
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#_pure_functions
+
+# A pure function does not modify its parameter
+function addtimeDEAD(t1, t2)
+    second = t1.second + t2.second
+    minute = t1.minute + t2.minute
+    hour = t1.hour + t2.hour
+
+    if second >= 60
+        second -= 60
+        minute += 1
+    end
+    if minute >= 60
+        minute -= 60
+        hour += 1
+    end
+
+    MyTime(hour, minute, second)
+end
+
+# Improved version
+function addtime(t1, t2)
+    totalsecond = timetosecond(t1) + timetosecond(t2)
+    secondtotime(totalsecond)
+end
+
+# Modifiers
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#modifiers
+"""
+Represents the time of day.
+
+hour:
+minute:
+second:
+"""
+mutable struct MMyTime
+    hour
+    minute
+    second
+end
+
+# Modifier version
+function increment!DEAD(time, seconds)
+    time.second += seconds
+
+    if time.second >= 60
+        time.second -= 60
+        time.minute += 1
+    end
+    if time.minute >= 60
+        time.minute -= 60
+        time.hour += 1
+    end
+    # Exercise 16-3
+    if time.second >= 60
+        increment!(time, 0)
+    end
+end
+
+# Exercise 16-5
+function increment!(time, seconds)
+    totalsecond = timetosecond(time) + seconds
+    newtime = secondtotime(totalsecond)
+
+    time.second = newtime.second
+    time.minute = newtime.minute
+    time.hour = newtime.hour
+end
+
+
+# Exercise 16-4: Pure version
+function increment(time, addsecond)
+    second = time.second + addsecond
+    minute = time.minute
+    hour = time.hour
+
+    if second >= 60
+        second -= 60
+        minute += 1
+    end
+    if minute >= 60
+        minute -= 60
+        hour += 1
+    end
+
+    rettime = MyTime(hour, minute, second)
+
+    if rettime.second >= 60
+    # Recursive to move second to minute to hour
+        rettime = increment(rettime, 0)
+    end
+
+    return rettime
+end
+
+# Prototyping Versus Planning
+# https://benlauwens.github.io/ThinkJulia.jl/latest/book.html#prototyping_versus_planning
+function timetosecond(time)
+    seconds = time.hour*3600 + time.minute*60 + time.second
+end
+
+function secondtotime(totalsecond)
+    (minute, second) = divrem(totalsecond, 60)
+    (hour, minute) = divrem(minute, 60)
+    return MyTime(hour, minute, second)
+end
+end
