e9eae9ce5f0e5942fd2760acfc3c69b87efd12b5
Yi-Kun Yang <ykyang@gmail.com>
2020-05-31 14:20:33 -0500

    JuliaAcademy: DataSciende: Data.jl done

--- JuliaAcademy/DataScience/Data.jl
+++ JuliaAcademy/DataScience/Data.jl
@@ -12,48 +12,73 @@ import NPZ
 import RData
 import MAT
 
-P = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv",
-    "programminglanguages.csv")
+# Download from network
+# pl: programming language
+pl_csv_filename = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv",
+     "programming_languages.csv")
+# This style save to a tmp file
+#filename = download("https://raw.githubusercontent.com/nassarhuda/easy_data/master/programming_languages.csv")
 
-# P: body, H: header
-P,H = readdlm("programminglanguages.csv", ','; header=true)
+# Read into arrays
+# Body, Header
+pl_body, pl_header = readdlm(pl_csv_filename, ','; header=true)
 
-# To write to a text file, you can:
-writedlm("programminglanguages_dlm.txt", P, '-')
+# Write to a text file with "-" as delimiter
+writedlm("programming_languages_dlm.txt", pl_body, "-")
 
 # CSV.jl
-C = CSV.read("programminglanguages.csv")
-show(C; allrows=true)
-@show typeof(C)
-describe(C)
+pl_csv = CSV.read(pl_csv_filename) # => DataFrame
+#show(pl_csv; allrows=true)
+@show typeof(pl_csv)
+@show describe(pl_csv)
 # column names
-names(C)
+@show names(pl_csv)
 # column
-C.year
-C.language
+pl_csv.year
+pl_csv.language
 
-# timing
+
+# Timing
+# comment out due to slow execution
 #@btime P,H = readdlm("programminglanguages.csv", ','; header=true)
 #@btime C = CSV.read("programminglanguages.csv")
 
-# write CSV
-df = DataFrame([P[:,1], P[:,2]],H[:])
-CSV.write("programminglanguages_CSV.csv", df)
-CSV.write("programminglanguages_CSV.csv", C)
+
+# Write CSV from arrays
+CSV.write("programming_languages_CSV.csv", pl_csv)
+
+
+# Notice the dot (.) operator below
+# Create DataFrame from arrays
+P1 = pl_body[:,1] # => Vector{Any}
+P1 = Int64.(P1)   # => Vector{Int64}
+P2 = pl_body[:,2] # => Vector{Any}
+P2 = String.(P2)  # => Vector{String}
+H = pl_header[:]
+pl_df = DataFrame([P1, P2], H)
+P1 = undef
+P2 = undef
+H = undef
+
 
 # Excel file
 # Excel file is super slow, convert to CSV
-T = XLSX.readdata("data/zillow_data_download_april2020.xlsx",
+city_xlsx = XLSX.readdata("data/zillow_data_download_april2020.xlsx",
     "Sale_counts_city", # sheet name
     "A1:F9" # cell range
     )
+
 # Does not work
 # G,L = XLSX.readdata("zillow_data_download_april2020.xlsx",
 #     "Sale_counts_city")
     #"A1:ER28760")
-G = CSV.read("zillow_data_download_april2020.csv")
 
-# Build my own DataFrame
+
+# Much faster
+city_csv = CSV.read("zillow_data_download_april2020.csv")
+
+
+# Build DataFrame from arrays
 foodvec = ["apple", "cucumber", "tomato", "banana"]
 calorievec = [105, 47, 22, 105]
 pricevec = [0.85, 1.6, 0.8, 0.6]
@@ -67,7 +92,7 @@ XLSX.writetable("food.xlsx", collect(eachcol(df)), names(df))
 
 # Read other data format
 
-# JLD
+# Julia
 jld_data = JLD.load("data/mytempdata.jld")
 JLD.save("mywrite.jld", "A", jld_data)
 #jld2 = JLD.load("mywrite.jld")
@@ -86,9 +111,59 @@ MAT.matwrite("mywrite.mat", mat_data)
 
 # Time to process the data from Julia
 function year_created(P, language::String)
+    println("Array version")
     filter = P[:,2] .== language
-    loc = findfirst(filter)
+    ind = findfirst(filter)
+    if isnothing(ind)
+        throw(MissingException("Language not found: $language"))
+    end
     #println("loc = $loc")
-    return P[loc,1]
+    return P[ind,1]
 end
-year_created(P, "Julia")
+year_created(pl_body, "Julia")
+#year_created(P, "not exist")
+
+function year_created(df::DataFrame, language::String)
+    println("DataFrame version")
+    loc = findfirst(df.language .== language)
+
+    if isnothing(loc)
+        e = MissingException("Language not found: $language")
+        throw(e)
+    end
+
+    return df.year[loc]
+end
+year_created(pl_df, "Julia")
+#year_created(pl_df, "W#")
+
+
+function count_by_year(df::DataFrame, year::Int64)
+    found_vec = findall(df.year .== year) # => Array{Int64,1}
+    yearcount = length(found_vec)
+
+    return yearcount
+end
+count_by_year(pl_df, 2011)
+
+
+# Dict with Any type
+D = Dict([("A", 1), ("B", 2), (1,[1,2])])
+
+# Dict with types
+pl_dic = Dict{Integer, Vector{String}}()
+# Populate pl_dic
+for i = 1:nrow(pl_df)
+    #size(pl_df,1)
+    #nrow(pl_df)
+    year,lang = pl_df[i,:]
+    if haskey(pl_dic, year)
+        vec = pl_dic[year]
+        push!(vec, lang)
+    else
+        pl_dic[year] = [lang]
+    end
+end
+
+
+# dropmissing
