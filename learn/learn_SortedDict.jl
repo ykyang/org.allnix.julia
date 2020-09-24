2133566abe774a465a31a6c08f33217672122e2f
Yi-Kun Yang <ykyang@gmail.com>
2020-09-24 09:20:54 -0500

    Update SortedDict

--- learn/learn_SortedDict.jl
+++ learn/learn_SortedDict.jl
@@ -3,12 +3,10 @@ logger = SimpleLogger(stdout, Logging.Info)
 global_logger(logger)
 
 using DataStructures
-#using Base.Order
 
 #>> Sort by char <<#
 
-# Base.Order.Reverse
-d = SortedDict{Char,Int}(Base.Order.Forward)
+d = SortedDict{Char,Int}(Base.Order.Forward) # Base.Order.Reverse
 
 d['c'] = 3
 d['d'] = 4
@@ -22,7 +20,8 @@ for p in col
     println("$(p[1]) $(p[2])")
 end
 
-#>> Sort by Date <<#
+#>> Sort by DateTime <<#
+
 using Dates
 d2 = SortedDict{DateTime, Int}(Base.Order.Forward)
 d2[now()] = 13
@@ -35,7 +34,9 @@ col2 = collect(d2)
 @info typeof(col2)
 @info col2
 
-#>> Dict of Dict and sort by Date <<#
+#>> Sort by Date <<#
+
+# SortedDict of Dict 
 d3 = SortedDict{Date, Dict}(Base.Order.Forward)
 
 d3[Date(2020, 12, 24)] = Dict("A"=>1)
@@ -44,7 +45,8 @@ d3[Date(2020, 12, 22)] = Dict("C"=>3)
 @info d3
 
 # Add item existing Dict
+# Indeed we are modifying a ref not a copy
 d3[Date(2020, 12, 23)]["D"] = 4
 @info d3
 
-nothing
+nothing # suppress printing the last line
