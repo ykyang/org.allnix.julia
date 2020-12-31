ad99c72619769e2f6b66d6f1b504ac8795fd4544
Yi-Kun Yang <ykyang@gmail.com>
2020-12-31 07:27:35 -0600

    Learn LsqFit

---
+++ learn/learn_LsqFit.jl
@@ -0,0 +1,26 @@
+using LsqFit
+
+model(t, p) = p[1] * exp.(-p[2] * t)
+
+tdata = range(0,10, length=20)
+ydata = model(tdata, [1.0, 2.0]) + 0.01*randn(length(tdata))
+
+p0 = [0.5, 0.5]
+
+fit = curve_fit(model, tdata, ydata, p0)
+
+using Plots
+plotly()
+
+pt = plot(title="Nonlinear Regression", size=(600,400))
+plot!(pt, tdata, ydata, seriestype=:scatter)
+
+a = fit.param[1]
+b = fit.param[2]
+fitmodel(t) = a * exp.(-b*t)
+#plot!(pt, fitmodel, extrema(tdata)..., seriestype=:line)
+x = range(0,10,length=100)
+y = @. fitmodel(x)
+plot!(pt, x, y, seriestype=:line)
+gui(pt)
+nothing
--- learn/learn_date.jl
+++ learn/learn_date.jl
@@ -20,10 +20,25 @@ df = dateformat"mm/dd/yyyy"
 d = Date(2020, 12, 29)
 d = d + Dates.Day(3)
 
+delta_day = Date(2020,10,31) - Date(2020, 10,20) 
+@info "Delta days = $delta_day"
+@info @show typeof(delta_day)
+@info @show typeof(delta_day.value)
+
+
 # Output
 d = Date(2020, 12, 29)
 @info typeof(d)
 # 21-Feb-2019
 str = Dates.format(d, "d-u-Y") # Why format(d, ...) does not work?
+
+# generate a list of date
+d = Date(2019, 03, 20)
+for ind in 1:730
+    global d
+    str = Dates.format(d, "d-u-Y")
+    println("DATE \"$str\"")
+    d += Day(1)
+end
 
 nothing # suppress last line printout
