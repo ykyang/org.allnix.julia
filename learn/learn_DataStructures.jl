139acdb7388e414a0f62963e85a304d553db1115
Yi-Kun Yang <ykyang@gmail.com>
2021-04-21 11:41:31 -0500

    Learn Set

--- learn/learn_DataStructures.jl
+++ learn/learn_DataStructures.jl
@@ -7,6 +7,29 @@ using Test
 import DataStructures
 
 
+function learn_Set()
+    set = Set{Int64}()
+
+    # isempty(s::Set) = isempty(s.dict)
+    # length(s::Set)  = length(s.dict)
+    # in(x, s::Set) = haskey(s.dict, x)
+    # push!(s::Set, x) = (s.dict[x] = nothing; s)
+    # pop!(s::Set, x) = (pop!(s.dict, x); x)
+    # pop!(s::Set, x, default) = (x in s ? pop!(s, x) : default)
+
+    push!(set, 1)
+    push!(set, 13)
+
+    @test in(1, set)
+    @test in(13, set)
+    @test !in(2, set)
+
+    x = pop!(set, 1)
+    @test 1 == x
+
+    @test !in(1, set)
+end
+
 function learn_Stack()
     ds = DataStructures
 
@@ -19,4 +42,7 @@ function learn_Stack()
     @test 1 == pop!(stack)
 end
 
-learn_Stack()
+
+
+#learn_Stack()
+learn_Set()
