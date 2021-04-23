156bd7632dcebb6db4437f82717def8b5f5f36e6
Yi-Kun Yang <ykyang@gmail.com>
2021-04-23 13:05:30 -0500

    clean

--- classic/ch4.jl
+++ classic/ch4.jl
@@ -14,28 +14,6 @@ Base.show(io::IO, x::Edge) = print(io, string(x))
     
 abstract type Graph{V,E} end
 
-# struct Graph{V,E}
-#     vertices::Vector{V}      # list of vertices
-#     # list of edges 
-#     # edges[i] -> list of edges that connects to vertices[i]
-#     edges::Vector{Vector{E}} 
-
-#     function Graph{V,E}() where {V,E}
-#         new{V,E}(Vector{V}(), Vector{Vector{E}}())
-#     end
-
-#     function Graph{V,E}(v::Vector{V}) where {V,E}
-#         me = new{V,E}(Vector{V}(), Vector{Vector{E}}())
-        
-#         append!(me.vertices, v)
-#         for i in 1:length(v)
-#             push!(me.edges, Vector{Vector{E}}())
-#         end
-
-#         return me
-#     end
-# end
-
 struct UnweightedGraph{V,E<:Edge} <: Graph{V,E}
     vertices::Vector{V}            # list of vertices
     edges_lists::Vector{Vector{E}} # edges_lists[i] -> list of edges that connects to vertices[i]
@@ -72,6 +50,20 @@ function add!(g::Graph{V,E}, edge::Edge) where {V,E<:Edge}
     push!(edges, reverse(edge))
 
     nothing
+end
+
+"""
+    Base.append!(g::Graph{V,E}, vertices) where {V,E<:Edge}
+
+Append vertices to a graph.  Creates empty edge lists for each vertex.
+"""
+function Base.append!(g::Graph{V,E}, vertices) where {V,E<:Edge} # is <:Edge necessary, could be more generic later
+    append!(g.vertices, vertices)
+
+    # One list of edges for each vertex
+    for i in 1:length(vertices)
+        push!(g.edges_lists, Vector{Vector{E}}())
+    end
 end
 
 function edges_of(g::Graph{V,E}, v::V) where {V,E<:Edge}
@@ -113,18 +105,7 @@ function vertex_at(g::Graph{V,E}, index::Int64) where {V,E<:Edge}
 end
 
 
-"""
 
-Append vertices to a graph.  Creates empty edge lists for each vertex.
-"""
-function Base.append!(g::Graph{V,E}, vertices) where {V,E<:Edge} # is <:Edge necessary, could be more generic later
-    append!(g.vertices, vertices)
-
-    # One list of edges for each vertex
-    for i in 1:length(vertices)
-        push!(g.edges_lists, Vector{Vector{E}}())
-    end
-end
 
 function Base.show(io::IO, g::Graph)
     for v in g.vertices
--- classic/test_ch4.jl
+++ classic/test_ch4.jl
@@ -4,6 +4,45 @@ using Test
 import .Classic
 cc = Classic
 
+function init!(g::cc.Graph{V,E}) where {V,E<:cc.Edge}
+    cities = Vector{String}(["Seattle", "San Francisco",  
+    "Los Angeles", "Riverside", "Phoenix", "Chicago", "Boston",
+    "New York", "Atlanta", "Miami", "Dallas", "Houston", "Detroit",
+    "Philadelphia", "Washington",
+    ])
+    
+    append!(g,cities)
+
+    cc.add!(g, "Seattle", "Chicago")
+    cc.add!(g, "Seattle","San Francisco")
+    cc.add!(g, "San Francisco", "Riverside")
+    cc.add!(g, "San Francisco", "Los Angeles")
+    cc.add!(g, "Los Angeles", "Riverside")
+    cc.add!(g, "Los Angeles", "Phoenix")
+    cc.add!(g, "Riverside", "Phoenix")
+    cc.add!(g, "Riverside", "Chicago")
+    cc.add!(g, "Phoenix", "Houston")
+    cc.add!(g, "Dallas", "Chicago")
+    cc.add!(g, "Dallas", "Atlanta")
+    cc.add!(g, "Dallas", "Houston")
+    cc.add!(g, "Houston", "Atlanta")
+    cc.add!(g, "Houston", "Miami")
+    cc.add!(g, "Atlanta", "Chicago")
+    cc.add!(g, "Atlanta", "Washington")
+    cc.add!(g, "Atlanta", "Miami")
+    cc.add!(g, "Miami", "Washington")
+    cc.add!(g, "Chicago", "Detroit")
+    cc.add!(g, "Detroit", "Boston")
+    cc.add!(g, "Detroit", "Washington")
+    cc.add!(g, "Detroit", "Washington")
+    cc.add!(g, "Detroit", "New York")
+    cc.add!(g, "Boston", "New York")
+    cc.add!(g, "New York", "Philadelphia")
+    cc.add!(g, "Philadelphia", "Washington")
+
+    return cities
+end
+
 function test_Edge()
     e = cc.Edge(2, 7)
     @test 2 == e.u
@@ -29,6 +68,7 @@ end
 function test_UnweightedGraph()
     @test_throws TypeError cc.UnweightedGraph{Int64,Int64}()
 
+    # V::Int64, E::Edge
     g = cc.UnweightedGraph{Int64,cc.Edge}()
     @test isempty(g.vertices)
     @test isempty(g.edges_lists)
@@ -38,11 +78,48 @@ function test_UnweightedGraph()
     @test [1,2,3] == g.vertices
     @test 3 == length(g.edges_lists)
 
+    # V::String, E::Edge
+    g = cc.UnweightedGraph{String,cc.Edge}()
+    @test isempty(g.vertices)
+    @test isempty(g.edges_lists)
+
+    g = cc.UnweightedGraph{String,cc.Edge}(["apple", "orange", "guava"])
+    @test 3 == length(g.vertices)
+    @test ["apple", "orange", "guava"] == g.vertices
+    @test 3 == length(g.edges_lists)
+
     # Learn types
     @test cc.UnweightedGraph{Int64,cc.Edge} <: cc.Graph{Int64,cc.Edge}
     @test isa(cc.UnweightedGraph{Int64,cc.Edge}(), cc.Graph{Int64,cc.Edge})
 end
 
-test_Edge()
+"""
+
+Sovle shortest route of `UnweightedGraph`
+"""
+function test_bfs()
+    g = cc.UnweightedGraph{String,cc.Edge}()
+    cities = init!(g)
+    @test length(cities) == length(g.vertices)
+    @test length(cities) == length(g.edges_lists)
+   
+    for edges in g.edges_lists
+        @test !isempty(edges)
+    end
+
+    show(g)
+
+    is_goal(pt) = cc.is_goal("Miami", pt)
+    neighbor_of(v) = cc.neighbor_of(g, v)
+
+    node = cc.bfs("Boston", is_goal, neighbor_of)
+    @test ["Boston", "Detroit", "Washington", "Miami"] == cc.node_to_path(node)
+    println("Shortest route: $(cc.node_to_path(node))")
+
+    nothing
+end
+
+#test_Edge()
 # test_Graph()
 test_UnweightedGraph()
+test_bfs()
