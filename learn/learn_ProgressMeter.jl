9e1703bda756e8f1303a17a4219def670d03da14
Yi-Kun Yang <ykyang@gmail.com>
2021-05-16 20:43:14 -0500

    Learn ProgressMeter

--- learn/learn_ProgressMeter.jl
+++ learn/learn_ProgressMeter.jl
@@ -1,8 +1,8 @@
 using ProgressMeter
 
 function learn_basic()
-    n = 100
-    p = Progress(n, dt=0.1, desc="Computing...", color=:black)
+    n = 50
+    p = Progress(n, dt=0.1, desc="Computing...", color=:green)
     for i in 1:n
         sleep(0.1)
         next!(p)
