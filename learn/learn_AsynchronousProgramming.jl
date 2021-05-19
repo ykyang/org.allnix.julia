84abc953a653710c74a427ce30014ffece9b452c
Yi-Kun Yang <ykyang@gmail.com>
2021-05-19 17:28:58 -0500

    Learn watch_file

--- learn/learn_AsynchronousProgramming.jl
+++ learn/learn_AsynchronousProgramming.jl
@@ -7,16 +7,16 @@ function learn_basic_task_operations()
         sleep(5)
         println("Done-1")
     end
-    task1 = Task(func)      # task
-    schedule(task1)         # run
+    task1 = Task(func)      # create task from a function
+    schedule(task1)         # run task
 
-    task2 = @task begin     # task
+    task2 = @task begin     # create task from code block
         sleep(4)
         println("Done-2")
     end
-    schedule(task2)         # run
+    schedule(task2)         # run task
 
-    task3 = @async begin    # task & run 
+    task3 = @async begin    # task & run in 1 step
         sleep(3)
         println("Done-3")
     end
@@ -26,7 +26,7 @@ end
 
 function learn_Channel()
     order_chn = Channel{Int}(32)
-    ship_chn= Channel{Tuple}(32)
+    ship_chn = Channel{Tuple}(32)
 
     n = 10
 
@@ -58,10 +58,49 @@ function learn_Channel()
     @async take_order()
 end
 
+function learn_file_task()
+    n = 100
+
+    writefile = function()
+        open("output.txt", "w") do io
+            for i in 1:n
+                sleep(0.1)
+                println(io, "Hello! Number $i")
+                flush(io)
+            end
+        end
+    end
+
+    readfile = function()
+        #st = watch_file("output.txt", 1)
+        #println("Timeout: $(st.timedout)")
+        st = (timedout=false,)
+        while !st.timedout
+            println("Here")
+            st = watch_file("output.txt", 10)
+            #if st.changed
+                open("output.txt", "r") do io
+                    for line in eachline(io)
+                        println("Read $line")
+                    end
+                end
+            #end
+            
+            println("Timedout: $(st.timedout)")
+        end
+    end 
+
+
+    @async writefile()
+    @async readfile()
+    #readfile()
+    
+end
 
 
 
 #learn_basic_task_operations()
-learn_Channel()
+#learn_Channel()
+learn_file_task()
 
 nothing
