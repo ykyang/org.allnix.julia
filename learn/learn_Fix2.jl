bf3c255a4db9a61ed74348f1ea034dac4216dc96
Yi-Kun Yang <ykyang@gmail.com>
2021-05-23 18:15:35 -0500

    Learn Fix2

--- learn/learn_Fix2.jl
+++ learn/learn_Fix2.jl
@@ -1,15 +1,32 @@
 using Test
+"""
+    add(a,b)
+
+Return `a + b`.
+
+Base library use this pattern to document variant of methods and define abstract
+function name.
+"""
+function add end
 
 function add(a,b)
     a+b
 end
 
 # A Fix2 defined for add for convenience
+"""
+    add(b)
+
+Create a function that adds its argument to `b`, i.e.
+a function equivalent to `f(x) = add(x,b)`.
+
+Base library use this pattern to document variant of methods.
+"""
 add(x) = Base.Fix2(add, x)
 
 function learn_Fix2()
     data = [1, 3, 5,  7, 11]
-    Ans  = [4, 6, 8, 10, 14]
+    Ans  = data .+ 3         # [4, 6, 8, 10, 14]
    
     # Using Fix2, concise 
     @test Ans == add(3).(data)
