c4313241dd82e3ac615798868a89e2f6d6b6408b
Yi-Kun Yang <ykyang@gmail.com>
2021-06-13 20:57:01 -0500

    Add Python stuff

---
+++ learn/learn_Conda.jl
@@ -0,0 +1,13 @@
+using Conda
+
+function learn_basic()
+    #@show Conda.ROOTENV
+    #Conda.runconda(`search python`)
+    #Conda.runconda(`create --name py39 python=3.9`)
+    #Conda.runconda(`env list`)
+end
+
+learn_basic()
+
+nothing
+
---
+++ learn/learn_PyCall.jl
@@ -0,0 +1,8 @@
+
+using Conda
+using PyCall
+import Pkg
+
+#py39 = joinpath(Conda.ROOTENV, "py39", "python.exe")
+#ENV["PYTHON"] = py39
+#Pkg.build("PyCall")
