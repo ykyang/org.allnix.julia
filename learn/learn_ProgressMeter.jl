6e141c6d7f5a522ef9735b6e11736466bdcf30f4
Yi-Kun Yang <ykyang@gmail.com>
2021-06-24 16:59:36 -0500

    Show value in ProgressMeter

--- learn/learn_ProgressMeter.jl
+++ learn/learn_ProgressMeter.jl
@@ -1,4 +1,5 @@
 using ProgressMeter
+using Dates
 
 function learn_basic()
     n = 50
@@ -7,8 +8,31 @@ function learn_basic()
         sleep(0.1)
         next!(p)
     end
+    finish!(p)
 end
 
-learn_basic()
+function learn_showvalues()
+    n = 50
+    p = Progress(n, dt=0.1, desc="Computing...", color=:green)
+    for i in 1:n
+        sleep(0.1)
+        next!(p, showvalues = [("Time",Dates.now())])
+    end
+    finish!(p)
+end
+
+function learn_update()
+    n = 50
+    p = Progress(n, dt=0.1, desc="Computing...", color=:green)
+    for i in 1:n
+        sleep(0.1)
+        ProgressMeter.update!(p, i, showvalues = [("Time",Dates.now())])
+    end
+    finish!(p)
+end
+
+#learn_basic()
+#learn_showvalues()
+learn_update()
 
 nothing
