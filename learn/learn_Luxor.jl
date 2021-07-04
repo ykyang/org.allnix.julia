a48f5e7ba6de074bf38570f14995d706947b3268
Yi-Kun Yang <ykyang@gmail.com>
2021-07-04 15:09:17 -0500

    Learn Luxor

--- learn/learn_Luxor.jl
+++ learn/learn_Luxor.jl
@@ -183,25 +183,157 @@ function learn_hello_jupyter(r, m)
 end
 
 ## http://juliagraphics.github.io/Luxor.jl/stable/tutorial/
-function learn_tutorial_first_steps()
+function learn_tutorial_first_steps_1()
     @svg begin
         text("Hello world")
         circle(O, 200, :stroke)
     end
 end
 
+function learn_tutorial_first_steps_2()
+    plt = @svg begin
+        text("Hello again, world!", Point(0,250))
+        circle(O, 200, :fill)
+        rulers()
+    end
 
+    return plt
+end
 
-# plt = learn_hello_world();               display(plt);
-# plt = learn_hello_circle();              display(plt);
-# plt = learn_hello_arrow();               display(plt); 
-# plt = learn_hello_julia_sector();        display(plt);
-# plt = learn_hello_pluto();               display(plt);
-# plt = learn_hello_julia_logos();         display(plt);
-# plt = learn_hello_sierpinski_triangle(); display(plt);
-# plt = learn_hello_jupyter(2, 0.95);      display(plt);
+function dot(P; r = 2)
+    circle(P, r, :fill)
+end
+
+## http://juliagraphics.github.io/Luxor.jl/stable/tutorial/#Euclidean-eggs
+function learn_euclidean_eggs()
+    plt = @svg begin
+        radius = 80
+        setdash("dot")
+        sethue("gray30") # does not affect the current opacity setting
+        A, B = [Point(x,0) for x in [-radius, radius]]
+        line(A, B, :stroke)
+        
+        # Circle O
+        # O == Point(0,0)
+        circle(O, radius, :stroke)
+
+        label("A", :NW, A)
+        label("O", :N,  O)
+        label("B", :NE, B)
+
+        dot.([A, O, B]) # black dots
+        circle.([A, B], 2*radius, :stroke) # big circle
+
+        # Intersection of circle A, B
+        count, C, D = intersectionlinecircle(Point(0, -2*radius), Point(0, 2*radius), A, 2*radius)
+
+        if 2 == count
+            #circle.([C,D], 2, :fill)
+            dot.([C,D])
+            label.(["C", "D"], :N, [C, D])
+        end
+
+        ## Upper circle
+        # C1, the tutorial use intersection but it is just `radius` above O
+        C1 = O + Point(0,-radius)
+        dot(C1,r=3)
+        label("C1", :N, C1)
+
+        # ip1, ip2
+        
+        src = B; dst = C1
+        count, I, J = intersectionlinecircle(src, dst, src, 2*radius)
+        dot.([I,J])
+        ip = distance(C1,I) < distance(C1,J) ? I : J
+        label("ip1", :N, ip)
+        ip1 = ip
+
+        src = A; dst = C1
+        count, I, J = intersectionlinecircle(src, dst, src, 2*radius)
+        dot.([I,J])
+        ip = distance(C1,I) < distance(C1,J) ? I : J
+        label("ip2", :N, ip)
+        ip2 = ip
+
+        circle(C1, distance(C1,ip2), :stroke)
+
+        ## Eggs at the ready
+        setline(5)
+        setdash("solid")
+
+        arc2r( B,   A, ip1, :path)
+        arc2r(C1, ip1, ip2, :path)
+        arc2r( A, ip2,   B, :path)
+        arc2r( O,   B,   A, :path)
+
+        strokepreserve() # stokepath()
+        setopacity(0.6)
+        sethue("ivory")
+        fillpath() # fillpreserve(), preserve the path after fill
+    end 700 700
+
+    return plt
+end
+
+function euclidean_egg(radius, action=:none)
+    A, B = [Point(x,0) for x in [-radius, radius]]
+    count, C, D = intersectionlinecircle(Point(0, -2*radius), Point(0, 2*radius), A, 2*radius)
+
+    C1 = O + Point(0,-radius)
+
+    src = B; dst = C1
+    count, I, J = intersectionlinecircle(src, dst, src, 2*radius)
+    ip = distance(C1,I) < distance(C1,J) ? I : J
+    ip1 = ip
+
+    src = A; dst = C1
+    count, I, J = intersectionlinecircle(src, dst, src, 2*radius)
+    ip = distance(C1,I) < distance(C1,J) ? I : J
+    ip2 = ip
+
+    newpath()
+    arc2r( B,   A, ip1, :path)
+    arc2r(C1, ip1, ip2, :path)
+    arc2r( A, ip2,   B, :path)
+    arc2r( O,   B,   A, :path)
+    closepath()
+
+    do_action(action)
+end
+
+function learn_euclidean_eggs_2()
+    plt = @svg begin
+        setopacity(0.7)
+        for theta in range(0, step=pi/6, length=12)
+            @layer begin # gsave() ... grestore()
+                rotate(theta)
+                translate(0,-150)
+                euclidean_egg(50, :path)
+                setline(10)
+                randomhue()
+                fillpreserve()
+
+                randomhue()
+                strokepath()
+            end
+        end
+    end 800 800
+end
+
+# plt = learn_hello_world();               display(plt)
+# plt = learn_hello_circle();              display(plt)
+# plt = learn_hello_arrow();               display(plt) 
+# plt = learn_hello_julia_sector();        display(plt)
+# plt = learn_hello_pluto();               display(plt)
+# plt = learn_hello_julia_logos();         display(plt)
+# plt = learn_hello_sierpinski_triangle(); display(plt)
+# plt = learn_hello_jupyter(2, 0.95);      display(plt)
 # TODO: Not finished yet
-plt = learn_tutorial_first_steps(); display(plt);
+
+# plt = learn_tutorial_first_steps_1(); display(plt)
+# plt = learn_tutorial_first_steps_2(); display(plt)
+# plt = learn_euclidean_eggs();         display(plt)
+plt = learn_euclidean_eggs_2();       display(plt)
 
 
 nothing
