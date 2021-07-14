a37f5d29e05b8f8902704b7a5e04d6e6fc80ab96
Yi-Kun Yang <ykyang@gmail.com>
2021-07-14 13:24:18 -0500

    Add PyCall tmp fix for
    pyimport("vtk") error

--- learn/learn_PyVtk.jl
+++ learn/learn_PyVtk.jl
@@ -19,38 +19,101 @@ if isnothing(findfirst(Conda.bin_dir(Conda.ROOTENV), ENV["PATH"])) && Sys.iswind
     ENV["PATH"] = Conda.bin_dir(Conda.ROOTENV) * ";" * ENV["PATH"]
 end
 
-vtk = pyimport("vtk")
+function learn_cone()
+    vtk = pyimport("vtk")
 
-colors = vtk.vtkNamedColors()
+    colors = vtk.vtkNamedColors()
 
-cone = vtk.vtkConeSource()
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputConnection(cone.GetOutputPort())
+    cone = vtk.vtkConeSource()
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(cone.GetOutputPort())
 
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetDiffuseColor(colors.GetColor3d("bisque"))
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetDiffuseColor(colors.GetColor3d("bisque"))
 
-renderer = vtk.vtkRenderer()
-
-
-
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(renderer)
+    renderer = vtk.vtkRenderer()
 
 
-renderer.AddActor(actor)
-renderer.SetBackground(colors.GetColor3d("Salmon"))
 
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(renderer)
 
-iren.Initialize()
-# We'll zoom in a little by accessing the camera and invoking a "Zoom"
-# method on it.
-#ren.ResetCamera()
-#ren.GetActiveCamera().Zoom(1.5)
-renWin.SetSize(300, 300)
-renWin.SetWindowName("Cone")
-renWin.Render()
-iren.Start()
+
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d("Salmon"))
+
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
+
+    iren.Initialize()
+    # We'll zoom in a little by accessing the camera and invoking a "Zoom"
+    # method on it.
+    #ren.ResetCamera()
+    #ren.GetActiveCamera().Zoom(1.5)
+    renWin.SetSize(300, 300)
+    renWin.SetWindowName("Cone")
+    renWin.Render()
+    iren.Start()
+end
+
+function boxCallback(obj, event)
+    vtk = pyimport("vtk") # Is this OK?
+    t = vtk.vtkTransform()
+    obj.GetTransform(t)
+    obj.GetProp3D().SetUserTransform(t)
+    # type(event): str
+    # event:InteractionEvent
+end
+
+function learn_box()
+    vtk = pyimport("vtk")
+
+    colors = vtk.vtkNamedColors()
+
+    # Create a Cone
+    cone = vtk.vtkConeSource()
+    cone.SetResolution(20)
+    coneMapper = vtk.vtkPolyDataMapper()
+    coneMapper.SetInputConnection(cone.GetOutputPort())
+    coneActor = vtk.vtkActor()
+    coneActor.SetMapper(coneMapper)
+    coneActor.GetProperty().SetColor(colors.GetColor3d("BurlyWood"))
+
+    # A renderer and render window
+    renderer = vtk.vtkRenderer()
+    renderer.SetBackground(colors.GetColor3d("Blue"))
+    renderer.AddActor(coneActor)
+
+    renwin = vtk.vtkRenderWindow()
+    renwin.AddRenderer(renderer)
+    
+
+    # An interactor
+    interactor = vtk.vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renwin)
+
+    # A Box widget
+    boxWidget = vtk.vtkBoxWidget()
+    boxWidget.SetInteractor(interactor)
+    boxWidget.SetProp3D(coneActor)
+    boxWidget.SetPlaceFactor(1.25)  # Make the box 1.25x larger than the actor
+    boxWidget.PlaceWidget()
+    boxWidget.On()
+
+    # Connect the event to a function
+    # This call makes cone move and stretch with the box.
+    # See complete list of event at 
+    # https://vtk.org/doc/nightly/html/vtkCommand_8h_source.html
+    boxWidget.AddObserver("InteractionEvent", boxCallback)
+
+    # Start
+    interactor.Initialize()
+    renwin.SetWindowName("BoxWidget")
+    renwin.Render()
+    interactor.Start()
+end
+
+
+#learn_cone()
+learn_box()
