e8cc4777c59aaa34f2cc735425c5e518a260fa29
Yi-Kun Yang <ykyang@gmail.com>
2021-09-18 10:14:25 -0500

    Move learn_for_loop.jl into learn_Julia.jl

--- learn/learn_Julia.jl
+++ learn/learn_Julia.jl
@@ -81,9 +81,71 @@ function learn_searchsortedfirst()
     @test 2 == i
 end
 
+"""
+
+Learn for-loop syntax
+```
+for i in 1:2, j in 3:4
+end
+```
+"""
+function learn_for_comma()
+    ijs = []
+
+    for i in 1:2, j in 3:4
+        push!(ijs, (i,j))
+    end
+
+    @test [
+        (1,3),
+        (1,4),
+        (2,3),
+        (2,4)
+    ] == ijs
+end
+
+"""
+    learn_comprehension()
+
+There are two ways to do two for-loops using comprehension.
+"""
+function learn_comprehension()
+    # List of pairs
+    x1 = [(i,j) for i in 1:2 for j in 3:4]
+    @test [
+        (1, 3), 
+        (1, 4), 
+        (2, 3), 
+        (2, 4)
+    ] == x1
+
+    # Matrix of pairs
+    x2 = [(i,j) for i in 1:2,j in 3:4]
+    @test [
+        (1, 3) (1, 4); 
+        (2, 3) (2, 4)
+    ] == x2
+
+    # Flatten
+    # Notice the order is different from x1
+    @test [(1, 3), (2, 3), (1, 4), (2, 4)] == x2[:]
+    @test [
+        (1, 3), 
+        (2, 3), 
+        (1, 4), 
+        (2, 4)
+    ] == vec(x2)
+end
+@testset "Node" begin
+    MyJulia.learn_Node()
+end
 @testset "Basic" begin
     learn_resize!()
     learn_searchsortedfirst()
+end
+@testset "For-Loop" begin
+    learn_for_comma()
+    learn_comprehension()
 end
 
 nothing
