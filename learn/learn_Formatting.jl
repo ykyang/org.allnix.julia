70bd097ab450fc69152dc75364bf71c40914973a
Yi-Kun Yang <ykyang@gmail.com>
2023-01-13 15:18:16 -0600

    Learn Formatting.jl

---
+++ learn/learn_Formatting.jl
@@ -0,0 +1,29 @@
+"""
+Learn `Formatting.jl`
+"""
+module LearnFormatting
+
+using Formatting
+using Test
+
+function learn()
+    ## String
+    @test sprintf1("%s", "This is a string") == "This is a string"
+    fmt = "%s" # @sprintf does not allow this, therefore the test
+    @test sprintf1(fmt, "This is a string") == "This is a string"
+
+    ## Float
+    @test sprintf1("%10.3f", 3.14159) == "     3.142"
+    
+    ## Integer
+    @test sprintf1("%d", 1000000)  == "1000000"
+    @test sprintf1("%'d", 1000000) == "1,000,000"
+
+
+end
+
+@testset "Basic" begin
+    learn()
+end
+
+end
