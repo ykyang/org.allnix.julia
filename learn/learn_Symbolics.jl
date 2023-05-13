e2a431e13b8501dbc171aa6caa81e422a95c6d95
Yi-Kun Yang <ykyang@gmail.com>
2023-05-13 13:47:51 -0500

    Learn Symbolics

--- learn/learn_Symbolics.jl
+++ learn/learn_Symbolics.jl
@@ -16,7 +16,7 @@ println(Ans)
 Ans = Symbolics.solve_for([x+y~0, x-y~2],[x,y])
 println(Ans)
 @test Ans == [1.0, -1.0]
-end
+
 
 
 ## https://symbolics.juliasymbolics.org/stable/examples/perturbation/
@@ -62,29 +62,57 @@ Collect coefficients of various powers of x's
 
 """
 function collect_powers(eqn, x, ns; max_power=100)
-    
-    ## Remove orders higher than ns
+    ## Remove orders higher than maximium of ns
     vars = Dict(x^j => 0 for j = maximum(ns)+1:max_power)
-    eqn = substitute(eqn, vars) # expand(eqn) outside of this function
+    eqn = substitute(eqn, vars) # call expand(eqn) outside of this function
 
-    coefs = Equation[]
+    coefs = Num[]
+    zero_powers = Dict(x^j => 0 for j=1:maximum(ns))
+    
     for i in ns
+        powers = zero_powers
+        #powers = copy(zero_powers)
+        powers[x^i] = 1
+        push!(coefs, substitute(eqn, powers))
+        powers[x^i] = 0
     end
 
+    return coefs
 end
 
 ## Solving the Quintic: x^5 + x = 1
 ## https://symbolics.juliasymbolics.org/stable/examples/perturbation/#Solving-the-Quintic
 let
-    n = 2
+    n = 10 # 2
     @variables ε a[1:n] a₀
     # ₀ ε
     a₀ = 1
-    x = a₀ + a[1]*ε + a[2]*ε^2
+    #x = a₀ + a[1]*ε + a[2]*ε^2 # x(ε)
+    x = a₀
+    for i = 1:n
+        x = x + a[i]*ε^i
+    end
+    @show x
     eqn = x^5 + ε*x - 1
-    @show expand(eqn)
+    #@show expand(eqn)
     #@show simplify(expand(eqn))
+    ε_coefs = collect_powers(expand(eqn), ε, 1:n) # coef of ε in terms of a's
+    # 1: 1 + 5a[1]
+    # 2: 10(a[1]^2) + 5a[2] + a[1]
+    #a_vals = Dict(a₀=>a₀) # store values of a₀, a[1], a[2] ...
+    a_vals = Dict() # store values of a₀, a[1], a[2] ...
+    for i in 1:length(a)
+        eq = substitute(ε_coefs[i], a_vals)
+        a_vals[a[i]] = Symbolics.solve_for(eq~0, a[i])
+    end
+
+    @show a_vals
+    @show substitute(x, a_vals)
+    y = substitute(x, a_vals)
+    @show substitute(y, Dict(ε=>1))
+
 end
 
+end
 
 nothing
