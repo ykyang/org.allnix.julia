0fff4129d9bdc7276a11da8429fbb9324a5c2277
Yi-Kun Yang <ykyang@gmail.com>
2023-06-04 13:03:04 -0500

    Reformat LearnGeometryForProgrammer.jl

--- learn/LearnGeometryForProgrammer.jl
+++ learn/LearnGeometryForProgrammer.jl
@@ -70,6 +70,49 @@ function learn_1_6()
     end
 end
 
+"""
+2.    Terminology and jargon
+2.1.1 Numbers
+      ℕ, natural number, do not include zero, 1, 2, 3, ...
+      ℕ0, ISO 80000-2, include zero, 0, 1, 2, ...
+      ℤ, integer
+      ℚ, rational
+      Irrational
+      ℝ, real number
+      Imaginary number
+      Complex number
+2.1.2 Vectors TODO
+2.2   Vertices and triangles
+2.2.1 Being pedantic about triangles
+2.2.2 Triangle quality TODO
+      Degenerate triangle, zero area, needle, cap
+      Manifold triangle mesh
+      Near-degenerate triangles
+      Rin/Rout metric
+2.3   Lines, planes, and their equations
+2.3.1 Lines and planes
+      General form, implicit
+      Canonical equation
+2.3.2 The parametric form for 3D lines
+      Parametric form
+2.4   Functions and geometric transformations
+2.4.1 What is a function?
+      Codomain, range
+2.4.2 Function types most commonly used in geometric modeling
+      Geometric transformation
+      Signed distance function
+2.5   The shortest possible introduction to matrix algebra
+2.5.1 What is algebra?
+      Isomorphism
+2.5.2 How does matrix algebra work?
+3.    The geometry of linear equations
+3.1   Linear equations as lines and planes
+      Definition of linear equation, solution
+
+
+"""
+
+
 end
 
 nothing
