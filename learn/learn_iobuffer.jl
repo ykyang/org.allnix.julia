091be44fc9bcda5ac212279c8dc3a73625de53ad
Yi-Kun Yang <ykyang@gmail.com>
2023-08-20 16:41:54 -0500

    Add IOBuffer test

--- learn/learn_iobuffer.jl
+++ learn/learn_iobuffer.jl
@@ -1,5 +1,7 @@
 # https://docs.julialang.org/en/v1/base/io-network/#Base.IOBuffer
 
+using Test
+
 # String -> IO
 io = IOBuffer()
 
@@ -10,5 +12,15 @@ x = take!(io)      # x is a byte array as shown below
 typeof(x)          # Array{UInt8,1}
 println(String(x)) # Convert to String
 
+
+text = """
+       Multiple line
+       paragraph
+       for testing
+       purpose
+       """
+io = IOBuffer()
+print(io, text)
+@test text == String(take!(io))
 
 
