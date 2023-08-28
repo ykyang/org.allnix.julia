43c2b9143f4537130f399678306a947d601a40b4
Yi-Kun Yang <ykyang@gmail.com>
2023-08-27 21:35:39 -0500

    Learn MLJ

---
+++ learn/learn_ISL_Pluto.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ac662ea0-44f8-11ee-36f4-c974b88f9249
+#
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.9.2"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═ac662ea0-44f8-11ee-36f4-c974b88f9249
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
