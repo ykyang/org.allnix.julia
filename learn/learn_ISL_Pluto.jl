b8127008ed77a11c14b54fe110380eca1a797492
Yi-Kun Yang <ykyang@gmail.com>
2023-09-01 11:19:33 -0500

    Learn ISL

--- learn/learn_ISL_Pluto.jl
+++ learn/learn_ISL_Pluto.jl
@@ -5,7 +5,54 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ ac662ea0-44f8-11ee-36f4-c974b88f9249
-#
+md"""
+# Introduction
+## An Overview of Statistical Learning
+
+* Data
+  * Wage
+  * Smarket
+  * NCI60
+
+* regression problem
+* classification problem
+* clustering problem
+
+## A Brief History of Statistical Learning
+* linear regression
+* linear discriminant analysis
+* logistic regression
+* generalized linear model
+* classification and regression trees
+* generalized additive models
+* neural networks
+"""
+
+
+# ╔═╡ e7bfbbab-738d-4088-8dbd-ba3cdd8c24b8
+md"""
+## This Book
+### Who Should Read This Book?
+### Notation and Simple Matrix Algebra
+* `n`: the number of distinct data points
+* `p`: the number of variables
+* ``x_{ij}``: ``j``th variable for the ``i``th observation
+* Matrix ``X``, see page 10
+### Organization of This Book
+* Ch2: K-nearest neighbor
+* Ch3: linear regression
+* Ch4: logistic regression, linear discriminant analysis
+* Ch5: cross-validation, bootstrap
+* Ch6: linear methods, stepwise selection, ridge regression, principal components, lasso
+* Ch7: non-linear additive models
+* Ch8: tree-based methods, bagging, boosting, random forests
+* Ch9: support vector machines
+* Ch10: deep learning
+* Ch11: survival analysis
+
+# Statistical Learning
+# Linear Regression
+"""
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -24,6 +71,7 @@ project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 """
 
 # ╔═╡ Cell order:
-# ╠═ac662ea0-44f8-11ee-36f4-c974b88f9249
+# ╟─ac662ea0-44f8-11ee-36f4-c974b88f9249
+# ╠═e7bfbbab-738d-4088-8dbd-ba3cdd8c24b8
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
